270019444964b08d6c7a285851315baef4e37ca4
sprite-enjoyer <samkharadze.saba01@gmail.com>
2023-02-12 12:41:27 +0400

    google verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="google-site-verification" content="wjpeI_KoPBnmi1kCFd_pDp_ylKntjhtCnCWjbhwPY2o" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="description" content="Saba Samkharadze - Fullstack/Frontend developer personal website" />
